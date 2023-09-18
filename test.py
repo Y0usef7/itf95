c9ac17eb660331cecf516eeb235ea51d6787836d
yousef <ynkoullp2001@gmail.com>
2023-09-18 13:58:21 +0300

    second commut

---
+++ test.py
@@ -0,0 +1,18 @@
+def sup(x,y):
+    total = x-y
+    return total
+def add(x,y):
+    total = x+y
+    return total
+def div(x,y):
+    total = x*y
+    return total
+def mul(x,y):
+    total = x/y
+    return total
+num1 =int(input("enter num 1"))
+num2 =int(input("enter num 2"))
+print(sup(num1,num2))
+print(add(num1,num2))
+print(div(num1,num2))
+print(mul(num1,num2))
