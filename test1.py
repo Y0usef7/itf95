4798eb030e1b4c823335b06336b7edebe1748ecd
yousef <ynkoullp2001@gmail.com>
2023-09-18 13:57:14 +0300

    second commut

---
+++ test1.py
@@ -0,0 +1,48 @@
+def sup(x,y):
+    total = x-y
+    print(total)
+def sup(x,y):
+    total = x-y
+    print(total)
+#print("yousef")
+#if 10 > 2:
+#    print("yes")
+#age = 30
+#m1 = "yousef"
+#print ("no")
+#input("enter number 1")
+#input("enter nuumber 2")
+#print(age)
+#print(m1)
+#number1 =input("enter number 1")
+#number2 =input("enter nuumber 2")
+#print(number1,number2)
+#var = False
+#var =int(var)ح
+#print(type(var))
+#print(var)
+
+#print(bool(10))
+#bool_val =False
+#print (type(bool_val))
+#a,s,d = 10,20,30
+#****************************************************
+#first_name=input("enter ferst name")
+#last_name =input("enter last name")
+#age = input(" enter age ")
+#n = input("enter nationality")
+#   تستخدم ال  F   لاعطاء قيمة للمتغير بدلا من طباعته كما هوة  مهمة
+#m = f"""
+#>>>name {first_name} {last_name}
+#>>>age {age}
+#>>>nationality {n}
+
+#"""
+#print(m)
+#"""    تستخدم لترتيب الجمل
+#*******************************************************
+#****************************************************************************
+email ="ynkoullp2001"
+print(email.replace(email[2:-2],"**********"))
+# يستخدم لتحويل الاحرف
+#****************************************************************************
